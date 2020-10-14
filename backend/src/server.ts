12b82a502b2abfd7a468d0ae956ab1c65676fabe
sanderdsz <sanderdsz@gmail.com>
2020-10-14 15:29:15 -0300

    typeorm and sqlite added, orphanage model created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,18 @@
-import express from 'express';
+import express, { response } from 'express';
+import './database/connection'
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/orphanages', (request, response) => {
+    return response.json({ message: 'Hello world' });
+});
+
 app.listen(3333);
+
+/*
+    yarn typeorm migration:create -n create_orphanages
+    yarn typeorm migration:run
+    yarn typeorm migration:revert
+*/
