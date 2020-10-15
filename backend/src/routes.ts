c9cde74e59e213fba63f9a063e307a509755268c
sanderdsz <sanderdsz@gmail.com>
2020-10-15 15:58:51 -0300

    controller abstraction

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import OrphanageController from './controllers/OrphanagesController'
+
+const routes = Router();
+
+routes.get('/orphanages', OrphanageController.index);
+routes.get('/orphanages/:id', OrphanageController.show);
+routes.post('/orphanages', OrphanageController.create);
+
+export default routes;
